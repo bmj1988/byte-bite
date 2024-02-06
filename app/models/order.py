37eea0ab7a279b479c5d344ad2005bf7f9e99b90
Brian <cjcores@gmail.com>
2024-02-05 22:55:27 -0500

    Added all models, including all relationships, going to commit and then run some seeders

---
+++ app/models/order.py
@@ -0,0 +1,43 @@
+from .db import db, environment, SCHEMA
+
+order_items = db.Table('order_items',
+    db.Column('order_id', db.Integer, db.ForeignKey('orders.id'), primary_key=True),
+    db.Column('menu_item_id', db.Integer, db.ForeignKey('menu_items.id'), primary_key=True))
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    status = db.Column(db.String(25), nullable=False)
+    driver = db.Column(db.String(25), nullable=False)
+    price = db.Column(db.Float(scale=2), nullable=False, default=0.00)
+
+    restaurant = db.relationship('Restaurant')
+    items = db.relationship('MenuItem', secondary=order_items, lazy=False)
+    user = db.relationship('User', back_populates="orders")
+
+    @property
+    def total(self):
+        return self.price
+
+
+    def add_price_of_item(self, price_of_item):
+        self.price += price_of_item
+        return self.price
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'customer': self.customer,
+            'restaurantId': self.restaurant_id,
+            'status': self.status,
+            'driver': self.driver,
+            'price': self.price,
+        }
