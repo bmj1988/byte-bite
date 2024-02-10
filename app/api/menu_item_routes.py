b09a45864dd8ec48f3dd95073bd8d30a17d59bad
Daniel <daniellegendre1221@gmail.com>
2024-02-10 16:05:17 -0500

    add post route for menu_item in restaurants, add menu_item_form

--- app/api/menu_item_routes.py
+++ app/api/menu_item_routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from flask_login import login_required, current_user
 from app.models import MenuItem, Restaurant, db
+from app.forms import MenuItemForm
 
 menu_item_routes = Blueprint('menu_item', __name__)
 
@@ -18,3 +20,25 @@ def get_menu_items_by_restaurant(restaurant_id):
         lst.append(entry)
 
     return dic
+
+@menu_item_routes.route('/<int:restaurant_id>/new', methods=['POST'])
+@login_required
+def post_new_menu_item_to_restaurant(restaurant_id):
+    form = MenuItemForm()
+    response = {}
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        data = form.data
+        new_menu_item = MenuItem(
+            name=data['name'],
+            price=data['price'],
+            image=data['image'],
+            description=data['description'],
+            restaurant_id=restaurant_id,
+        )
+        db.session.add(new_menu_item)
+        db.session.commit()
+        return new_menu_item.to_dict()
+    response['formErrors'] = form.errors
+    return response, 400
