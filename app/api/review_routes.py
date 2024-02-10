e7c5d3eec1177375f4391b3507e74fb17bcc4ddc
Gerrod White <gerrodww@gmail.com>
2024-02-10 14:54:24 -0500

    get reviews by user id route tested

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
+from flask_login import current_user, login_required
 from app.models import Review, db
 
 review_routes = Blueprint('review', __name__)
 
+# get reviews by restaurant_id
 @review_routes.route('/<int:restaurant_id>')
 def reviews_by_rest(restaurant_id):
   reviews_by_restaurant = db.session.query(Review).filter_by(restaurant_id=restaurant_id).all()
@@ -13,6 +15,23 @@ def reviews_by_rest(restaurant_id):
   lst = list()
   dic = {"reviews": lst}
   for review in reviews_by_restaurant:
+    rev_entry = review.to_dict()
+    lst.append(rev_entry)
+
+  return dic
+
+# get reviews by current user
+@review_routes.route('/current')
+@login_required
+def reviews_by_user():
+  reviews_by_user_id = db.session.query(Review).filter_by(user_id=current_user.id).all()
+
+  if not reviews_by_user_id:
+    return {'message': 'Current user does not have any reviews'}
+  
+  lst = list()
+  dic = {"reviews": lst}
+  for review in reviews_by_user_id:
     rev_entry = review.to_dict()
     lst.append(rev_entry)
 
