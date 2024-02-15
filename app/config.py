be5d8632e071f68334a58815168f5452527daa05
Brian <cjcores@gmail.com>
2024-02-15 15:57:22 -0500

    committing to save the work for the store page

--- app/config.py
+++ app/config.py
@@ -10,5 +10,5 @@ class Config:
     # url in the hidden config vars to start with postgres.
     # so the connection uri must be updated here (for production)
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
+        '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
