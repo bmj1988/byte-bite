bdc6557f0370d51cf5c538af6145ef806a4016ef
Brian <cjcores@gmail.com>
2024-02-15 20:15:45 -0500

    Completing merge

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -30,7 +30,6 @@ export const router = createBrowserRouter([
       },
       {
         path: "/update",
-        path: "/update",
         children: [
           {
             path: ':restaurantId',
