3ef264dd77d3b8a815bd085c3b584bb5d92a46bf
Brian <cjcores@gmail.com>
2024-02-17 13:11:10 -0500

    Saving progress to merge with main

---
+++ frontend/src/components/StorePage/utils/iconsForReviewBox.jsx
@@ -0,0 +1,12 @@
+import { FaAppleAlt, FaLemon, FaCarrot, FaLeaf, FaSeedling, FaPepperHot } from "react-icons/fa"
+
+const icons = [
+    <FaAppleAlt style={{ fontSize: '25px', color: 'red' }} />,
+    <FaSeedling style={{ fontSize: '25px', color: 'green' }} />,
+    <FaCarrot style={{ fontSize: '25px', color: 'orange' }} />,
+    <FaLeaf style={{ fontSize: '25px', color: 'green' }} />,
+    <FaPepperHot style={{ fontSize: '25px', color: 'red' }} />,
+    <FaLemon style={{ fontSize: '25px', color: 'yellow' }} />
+]
+
+export default icons
